37ae25211653407688349b2659c4f90fd0e0e519
Hugo van Duijn <hugo.vanduijn@naturalis.nl>
2015-01-27 10:55:43 +0000

    initial commit of role_ccw puppet manifest

---
+++ manifests/init.pp
@@ -0,0 +1,71 @@
+# == Class: role_ccw
+#
+#
+# === Authors
+#
+# Author Name <hugo.vanduijn@naturalis.nl>
+#
+#
+class role_ccw (
+  $docroot                    = '/var/www/htdocs',
+  $webdirs                    = ['/var/www/htdocs'],
+  $rwwebdirs                  = ['/var/www/htdocs/cache'],
+  $enable_mysql               = undef,
+  $mysql_root_password        = 'rootpassword',
+  $mysql_manage_config_file   = 'true',
+  $instances                  = {'site.lampsite.nl' => {
+                           'serveraliases'   => '*.lampsite.nl',
+                           'docroot'         => '/var/www/htdocs',
+                           'directories'     => [{ 'path' => '/var/www/htdocs', 'options' => '-Indexes +FollowSymLinks +MultiViews', 'allow_override' => 'All' }],
+                           'port'            => 80,
+                           'serveradmin'     => 'webmaster@naturalis.nl',
+                           'priority'        => 10,
+                          },
+                         },
+){
+
+    file { $webdirs:
+      ensure         => 'directory',
+      mode           => '0750',
+      owner          => 'root',
+      group          => 'www-data',
+      require        => Class['apache']
+    }->
+    file { $rwwebdirs:
+      ensure         => 'directory',
+      mode           => '0777',
+      owner          => 'www-data',
+      group          => 'www-data',
+      require        => File[$webdirs]
+    }
+  
+
+# install php module php-gd
+  php::module { [ 'gd','mysql','curl' ]: }
+
+# Install apache and enable modules
+  class { 'apache':
+    default_mods     => true,
+    mpm_module       => 'prefork',
+  }
+  include apache::mod::php
+  include apache::mod::rewrite
+  include apache::mod::speling
+
+# Create instances (vhosts)
+  class { 'role_lamp::instances': 
+      instances      => $instances,
+  }
+
+# Configure MySQL Security
+  if $enable_mysql {
+    class { 'mysql::server::account_security':}
+    class { 'mysql::server':
+        root_password   => $mysql_root_password,
+        service_enabled => true,
+        service_manage  => true,
+        manage_config_file  => $mysql_manage_config_file,
+    }
+  }
+  
+}
